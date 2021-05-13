7a8528c19da283250ce768e0c6db848344ccdf14
mdemon420 <mdeamon4@gmail.com>
2021-05-14 01:57:31 +0600

    undefined

---
+++ undefined.js
@@ -0,0 +1,24 @@
+let name;
+console.log(name);
+
+ function add(num1,num2){
+    console.log(num1+num2);
+ }
+
+ const result = add(20,30);
+ console.log(result);
+
+ function add2(num1,num2){
+    return
+ }
+
+ const result2 = add2(21,31);
+ console.log(result2);
+
+ function add3(num1,num2){
+    return num1 + num2;
+ }
+
+ const result3 = add3(21);
+ console.log(result3);
+
