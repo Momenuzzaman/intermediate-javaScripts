fb76af0418c43e0abcdc314e5e6fc37eacf6ece5
mdemon420 <mdeamon4@gmail.com>
2021-05-19 01:36:37 +0600

    map and filter

--- map-filter.js
+++ map-filter.js
@@ -1,9 +1,29 @@
-const numbers = [2,3,4,5,6,7,8,9,20];
-const output = []
+const numbers = [2,3,4,5,6,7,8,9,10];
+// const output = []
 
-for(let i = 0; i < numbers.length; i++){
-    const number = numbers[i];
-    const result = number * number;
-    output.push(result);
-}
-console.log(output);
+// // for(let i = 0; i < numbers.length; i++){
+// //     const number = numbers[i];
+// //     const result = number * number;
+// //     output.push(result);
+// // }
+// // console.log(output);
+
+// map
+ function square(element){
+   return element*element;
+ }
+ const result = numbers.map(function(element){
+    return element*element;
+});
+console.log(result);
+
+const square2 = element=> element*element;
+const result2 = numbers.map(element=> element*element);
+console.log(result2);
+
+
+
+
+// filter
+const bigger = numbers.filter(element => element > 5);
+console.log(bigger);
